4780a3d81c5269ca7ad3ee84e6a53309799139b0
Serex <adanjogohe@gmail.com>
2023-03-25 10:12:57 +0000

    Example of lambda and removed tracking of idea files

---
+++ src/main/java/com/adangomez/learning/ExampleOfLambda.java
@@ -0,0 +1,20 @@
+package com.adangomez.learning;
+
+public class ExampleOfLambda {
+
+    public static void start(){
+
+        // This is an example of a full body expression
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from normal thread");
+            }
+        });
+        t.start();
+
+        // This is an example of a lambda expression
+        new Thread(() -> System.out.println("Hello from lambda thread")).start();
+
+    }
+}
