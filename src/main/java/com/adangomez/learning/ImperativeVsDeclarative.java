58b2f3dc0fec6d36c9255f805caa88ae2cf0d25a
Serex <adanjogohe@gmail.com>
2023-03-25 09:45:08 +0000

    Imperative vs Declarative showdown

---
+++ src/main/java/com/adangomez/learning/ImperativeVsDeclarative.java
@@ -0,0 +1,32 @@
+package com.adangomez.learning;
+
+import java.util.stream.IntStream;
+
+public class ImperativeVsDeclarative {
+
+    public static void start(){
+        System.out.println("Imperative vs Declarative");
+        System.out.println("Imperative: ");
+
+        int sumOfEvents = 0;
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                sumOfEvents += i;
+            }
+        }
+
+        System.out.println(sumOfEvents);
+
+        System.out.println("Declarative: ");
+
+        sumOfEvents = IntStream.rangeClosed(0, 100)
+                .filter(i -> i % 2 == 0)
+                .reduce(Integer::sum)
+                .getAsInt();
+
+        System.out.println(sumOfEvents);
+
+    }
+
+}
--- src/main/java/com/adangomez/learning/Main.java
+++ src/main/java/com/adangomez/learning/Main.java
@@ -3,7 +3,7 @@ package com.adangomez.learning;
 public class Main {
     public static void main(String[] args) {
 
-
+        ImperativeVsDeclarative.start();
 
     }
 }
